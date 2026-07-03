90fd46b9c94075769234071db11cd873c250d828
vuloi05 <loilinhvu@gmail.com>
2026-07-03 21:33:57 +0700

    update USBD_CUSTOM_HID_REPORT_DESC_SIZE

--- USB_DEVICE/Target/usbd_conf.h
+++ USB_DEVICE/Target/usbd_conf.h
@@ -77,7 +77,7 @@
 /*---------- -----------*/
 #define USBD_CUSTOMHID_OUTREPORT_BUF_SIZE     2U
 /*---------- -----------*/
-#define USBD_CUSTOM_HID_REPORT_DESC_SIZE     2U
+#define USBD_CUSTOM_HID_REPORT_DESC_SIZE     58U
 /*---------- -----------*/
 #define CUSTOM_HID_HS_BINTERVAL     1U
 
