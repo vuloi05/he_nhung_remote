0a0785c3e34f5ba95f0d80373c62ec28638444f7
duyhung2801 <hungduytran11@gmail.com>
2026-07-03 22:33:03 +0700

    usb hid gamepad

---
+++ Core/Inc/gamepad_hid.h
@@ -0,0 +1,18 @@
+#ifndef GAMEPAD_HID_H
+#define GAMEPAD_HID_H
+
+#include <stdint.h>
+
+// Struct 7 bytes: Trình tự phải giống y hệt thứ tự mô tả trong Report Descriptor
+typedef struct __attribute__((packed)) {
+    uint8_t  buttons;    // Bit 0->7: 8 nút bấm
+    uint8_t  left_x;     // 0=Left, 128=Center, 255=Right
+    uint8_t  left_y;     // 0=Up, 128=Center, 255=Down
+    uint8_t  right_x;    // 0=Left, 128=Center, 255=Right
+    uint8_t  right_y;    // 0=Up, 128=Center, 255=Down
+    int16_t  gyro_raw;   // Dữ liệu Gyro (-32768 đến 32767)
+} GamepadReport_t;
+
+void Gamepad_SendReport(GamepadReport_t *report);
+
+#endif // GAMEPAD_HID_H
